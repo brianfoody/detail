c9df20ef5a5fa0d8f0c93734b85fdd2a7d7f6927
Brian Foody <brianfoody@gmail.com>
2017-01-12 15:44:39 +0800

    ANother change from the master

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,1 @@
-console.log("This is a store - now inside another module. Lets see if change gets picked up.")
+console.log("This is a store - now inside another module. Lets see if change gets picked up. Now lets change in the master.")
